1f58237be26d6f910c59d80e5d361b4ddcb6a0c5
ali baba <michael@cheneval.com>
2024-05-21 13:01:35 +0200

    Sentiers

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/sentiers">Sentiers</RouterLink>
       </nav>
     </div>
   </header>
