02774b8427a2c653ac38d0df95e32a126a0d628a
siphore <michael@cheneval.com>
2024-05-27 15:21:11 +0200

    Maybe ?

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/appStroll.js'
             ],
             refresh: true,
         }),
