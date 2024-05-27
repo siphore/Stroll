d8828c6974db316bc8957d25952478fdc6b2b992
siphore <michael@cheneval.com>
2024-05-27 14:28:13 +0200

    Redirect

--- laravel/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ laravel/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended('/map');
     }
 
     /**
@@ -42,6 +42,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect('/map');
     }
 }
