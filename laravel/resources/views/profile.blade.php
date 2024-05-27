4df63b55c572f1c90ad3ae259a23f5d5a3529f83
ali baba <michael@cheneval.com>
2024-05-27 15:31:35 +0200

    Profile v2

--- laravel/resources/views/profile.blade.php
+++ laravel/resources/views/profile.blade.php
@@ -4,9 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Profile</title>
-        @vite('resources/js/appStroll.js')
+        <!-- @vite('resources/js/appStroll.js') -->
     </head>
     <body>
-        <div id="profile"></div>
+        <div id="profile">
+            <h1>Profile</h1>
+        </div>
     </body>
 </html>
