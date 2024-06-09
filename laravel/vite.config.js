0c3274274166b16c60b995db59772efd0e092ee3
ali baba <michael@cheneval.com>
2024-06-09 16:21:05 +0200

    Icon lol

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -3,44 +3,55 @@ import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
+export default () => {
+    const manifestIcons = [
+        {
+            src: "/Logo.png",
+            sizes: "224x192",
+            type: "image/png",
+        },
+    ];
+
+    return defineConfig({
+        plugins: [
+            laravel({
+                input: ["resources/css/app.css", "resources/js/app.js"],
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
                 },
-            },
-        }),
-        VitePWA({
-            buildBase: '/build/',
-            scope: '/',
-            base: '/',
-            registerType: "autoUpdate",
-            includeAssets: [],
-            devOptions: {
-                enabled: false
-            },
-            manifest: {
-                name: 'VDiscover',
-                short_name: 'vdiscover',
-                description: 'Sentiers culturels du canton de Vaud',
-                theme_color: '#DE9918',
-                background_color: '#DE9918',
-                orientation: 'portrait',
-                display: 'standalone',
-                scope: '/',
-                start_url: '/',
-                id: '/',
-            },
-            workbox: {
-                globPatterns: ["**/*.{js,css,html,png,jpg,jpeg,svg}"],
-            },
-        }),
-    ],
-});
+            }),
+            VitePWA({
+                buildBase: "/build/",
+                scope: "/",
+                base: "/",
+                registerType: "autoUpdate",
+                includeAssets: [],
+                devOptions: {
+                    enabled: false,
+                },
+                manifest: {
+                    name: "VDiscover",
+                    short_name: "vdiscover",
+                    description: "Sentiers culturels du canton de Vaud",
+                    theme_color: "#DE9918",
+                    background_color: "#DE9918",
+                    orientation: "portrait",
+                    display: "standalone",
+                    scope: "/",
+                    start_url: "/",
+                    id: "/",
+                    icons: [...manifestIcons],
+                },
+                workbox: {
+                    globPatterns: ["**/*.{js,css,html,png,jpg,jpeg,svg}"],
+                },
+            }),
+        ],
+    });
+};
