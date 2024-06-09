7016a862688bc3f691795135e918df261bf6cd3d
ali baba <michael@cheneval.com>
2024-06-09 15:05:01 +0200

    PWA pls

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -24,6 +24,36 @@ if ("serviceWorker" in navigator) {
     });
 }
 
-// navigator.serviceWorker.register(new URL("worker.js", import.meta.url));
+let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+    // Prevent the mini-infobar from appearing on mobile
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI to notify the user they can add to home screen
+    showInstallPromotion();
+});
+
+function showInstallPromotion() {
+    const installButton = document.getElementById("installButton");
+    installButton.style.display = "block";
+
+    installButton.addEventListener("click", (e) => {
+        // Hide the install promotion
+        installButton.style.display = "none";
+        // Show the install prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === "accepted") {
+                console.log("User accepted the A2HS prompt");
+            } else {
+                console.log("User dismissed the A2HS prompt");
+            }
+            deferredPrompt = null;
+        });
+    });
+}
 
 createApp(App).mount("#app");
--- laravel/webpack.mix.js
+++
@@ -1,5 +0,0 @@
-const mix = require("laravel-mix");
-
-mix.js("resources/js/app.js", "public/js")
-    .css("resources/css/filters.css", "public/css")
-    .setPublicPath("public");
